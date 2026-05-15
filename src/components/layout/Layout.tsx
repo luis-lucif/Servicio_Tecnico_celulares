0a611b4642cc40638f3fecdc05d8ad12e5e9c100
luis-lucif <empluis@hotmail.com>
2026-05-14 22:42:33 -0300

    Remove BottomNav and ConnectionTest, add WhatsApp quote sharing

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-import BottomNav from './BottomNav';
 
 export default function Layout() {
   return (
@@ -9,7 +8,6 @@ export default function Layout() {
       <Header />
       <Outlet />
       <Footer />
-      <BottomNav />
     </>
   );
 }
